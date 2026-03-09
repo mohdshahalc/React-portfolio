f80355df56e01600ea033ff50ea3333c63a1e7bb
shahal <shahal.c077@gmail.com>
2026-03-09 11:51:32 +0530

    Fix API URL

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
     setStatus("");
 
     try {
-      const response = await fetch("http://localhost:5000/api/contact", {
+      const response = await fetch("https://react-portfolio-co3w.onrender.com/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
